d05f21a1735ffb44ce83edbc816c83b49a868a02
lehongdanh99 <lehongdanh99@gmail.com>
2022-02-12 16:38:53 +0700

    Create Project
    Create project

---
+++ POS-Coffee/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace POS_Coffee.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
