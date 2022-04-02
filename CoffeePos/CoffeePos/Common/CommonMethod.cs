77c51b300708e1e83379f977a103a9ff19212912
lehongdanh99 <lehongdanh99@gmail.com>
2022-04-02 22:59:02 +0700

    readjson file

--- CoffeePos/CoffeePos/Common/CommonMethod.cs
+++ CoffeePos/CoffeePos/Common/CommonMethod.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using CoffeePos.Models;
+using DocumentFormat.OpenXml.Office.CustomUI;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
 
 namespace CoffeePos.Common
 {
-    class CommonMethod
+    public class CommonMethod
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static CommonMethod _instance;
+        private TableModel model = new TableModel();
+        public static CommonMethod GetInstance()
+        {
+            if (_instance == null)
+            {
+                if (_instance == null)
+                {
+                    _instance = new CommonMethod();
+                }
+            }
+            return _instance;
+        }
+
+        public TableModel readJsonFileConfig()
+        {
+            string json = String.Empty;
+            using (StreamReader r = new StreamReader("TableConfig.json"))
+            {
+                json = r.ReadToEnd();
+                model = JsonConvert.DeserializeObject<TableModel>(json);
+            }
+            return model;
+            log.Info($"Read file Table config to Table model {json.ToString()} ");
+        }   
     }
 }
