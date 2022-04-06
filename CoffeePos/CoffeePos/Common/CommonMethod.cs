bfaef35e5e7d60ea525fbfcbaf272a5beb7cf4d9
lehongdanh99 <lehongdanh99@gmail.com>
2022-04-06 22:33:30 +0700

    update table models

--- CoffeePos/CoffeePos/Common/CommonMethod.cs
+++ CoffeePos/CoffeePos/Common/CommonMethod.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using CoffeePos.Models;
+using DocumentFormat.OpenXml.Office.CustomUI;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
 
 namespace CoffeePos.Common
 {
-    class CommonMethod
+    public class CommonMethod
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static CommonMethod _instance;
+        private TableModel model = new TableModel();
+        public static CommonMethod GetInstance()
+        {
+            if (_instance == null)
+            {
+                if (_instance == null)
+                {
+                    _instance = new CommonMethod();
+                }
+            }
+            return _instance;
+        }
+
+        public TableModel readJsonFileConfig()
+        {
+            string json = String.Empty;
+            using (StreamReader r = new StreamReader(GlobalDef.JSON_CONFIG_PATH))
+            {
+                json = r.ReadToEnd();
+                model = JsonConvert.DeserializeObject<TableModel>(json);
+            }
+            log.Info($"Read file Table config to Table model {json.ToString()} ");
+            return model;
+        }
     }
 }
+
+
--- CoffeePos/CoffeePos/ViewModels/TablesViewModel.cs
+++ CoffeePos/CoffeePos/ViewModels/TablesViewModel.cs
@@ -1,213 +1,213 @@
-﻿using Caliburn.Micro;
-using CoffeePos.Models;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Media;
+﻿//using Caliburn.Micro;
+//using CoffeePos.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Collections.ObjectModel;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Media;
 
-namespace CoffeePos.ViewModels
-{
-    internal class TablesViewModel : Screen
-    {
-        bool isChoose;
-        int floor = 1;
-        public SelectedTableThis eventChooseTableToOrder;
-        public delegate void SelectedTableThis(int SelectedTable);
+//namespace CoffeePos.ViewModels
+//{
+//    internal class TablesViewModel : Screen
+//    {
+//        bool isChoose;
+//        int floor = 1;
+//        public SelectedTableThis eventChooseTableToOrder;
+//        public delegate void SelectedTableThis(int SelectedTable);
 
-        public TablesViewModel(bool isChooseTable)
-        {
-            isChoose = isChooseTable;
-            TablesAllList = GetAllTableList();
-            GetStatusAllTableList(TablesAllList);
-            ListFloor = GetListFloor();
-            TablesList = GetTableList(listFloorSelected, TablesAllList);
-        }
+//        public TablesViewModel(bool isChooseTable)
+//        {
+//            isChoose = isChooseTable;
+//            TablesAllList = GetAllTableList();
+//            GetStatusAllTableList(TablesAllList);
+//            ListFloor = GetListFloor();
+//            TablesList = GetTableList(listFloorSelected, TablesAllList);
+//        }
 
-        private void GetStatusAllTableList(ObservableCollection<Table> listTable)
-        {
-            EmtyCount = 0;
-            BusyCount = 0;
-            for (int i=0; i< listTable.Count; i++)
-            {
-                if (listTable[i].Status)
-                {
-                    EmtyCount++;
-                }
-                else
-                    BusyCount++;
-            }
-            NotifyOfPropertyChange(() => EmtyCount);
-            NotifyOfPropertyChange(() => BusyCount);
-        }
+//        //private void GetStatusAllTableList(ObservableCollection<Table> listTable)
+//        //{
+//        //    EmtyCount = 0;
+//        //    BusyCount = 0;
+//        //    for (int i=0; i< listTable.Count; i++)
+//        //    {
+//        //        if (listTable[i].Status)
+//        //        {
+//        //            EmtyCount++;
+//        //        }
+//        //        else
+//        //            BusyCount++;
+//        //    }
+//        //    NotifyOfPropertyChange(() => EmtyCount);
+//        //    NotifyOfPropertyChange(() => BusyCount);
+//        //}
 
-        private ObservableCollection<int> GetListFloor()
-        {
-            ObservableCollection<int> listFloors = new ObservableCollection<int>();
-            for(int i = 0; i < TablesAllList.Count; i++)
-            {
-                if(listFloors.Count != 0)
-                {
-                    bool isFloor = false;
-                    for(int j = 0; j < listFloors.Count; j++)
-                    {
-                        if (TablesAllList[i].Floor == listFloors[j])
-                            isFloor = true;
-                    }
-                    if(!isFloor)
-                        listFloors.Add(TablesAllList[i].Floor) ;
-                }
-                else
-                    listFloors.Add(TablesAllList[i].Floor);
+//        private ObservableCollection<int> GetListFloor()
+//        {
+//            ObservableCollection<int> listFloors = new ObservableCollection<int>();
+//            for(int i = 0; i < TablesAllList.Count; i++)
+//            {
+//                if(listFloors.Count != 0)
+//                {
+//                    bool isFloor = false;
+//                    for(int j = 0; j < listFloors.Count; j++)
+//                    {
+//                        if (TablesAllList[i].Floor == listFloors[j])
+//                            isFloor = true;
+//                    }
+//                    if(!isFloor)
+//                        listFloors.Add(TablesAllList[i].Floor) ;
+//                }
+//                else
+//                    listFloors.Add(TablesAllList[i].Floor);
                 
-            }
+//            }
 
-            return listFloors;
-        }
+//            return listFloors;
+//        }
 
-        private int emtycount;
-        public int EmtyCount
-        { get { return emtycount; }
-            set
-            {
-                emtycount = value;
-                NotifyOfPropertyChange(() => EmtyCount);
-            } }
+//        private int emtycount;
+//        public int EmtyCount
+//        { get { return emtycount; }
+//            set
+//            {
+//                emtycount = value;
+//                NotifyOfPropertyChange(() => EmtyCount);
+//            } }
 
-        private int busycount;
-        public int BusyCount
-        {
-            get { return busycount; }
-            set
-            {
-                busycount = value;
-                NotifyOfPropertyChange(() => BusyCount);
-            }
-        }
+//        private int busycount;
+//        public int BusyCount
+//        {
+//            get { return busycount; }
+//            set
+//            {
+//                busycount = value;
+//                NotifyOfPropertyChange(() => BusyCount);
+//            }
+//        }
 
-        private int _selectedIndexTable = -1;
-        public int SelectedIndexTable
-        {
-            get
-            {
-                return _selectedIndexTable;
-            }
+//        private int _selectedIndexTable = -1;
+//        public int SelectedIndexTable
+//        {
+//            get
+//            {
+//                return _selectedIndexTable;
+//            }
 
-            set
-            {
+//            set
+//            {
 
-                _selectedIndexTable = value;
-                if(_selectedIndexTable >=0)
-                {
-                    btTableSelected_Click(_selectedIndexTable);
-                }
+//                _selectedIndexTable = value;
+//                if(_selectedIndexTable >=0)
+//                {
+//                    btTableSelected_Click(_selectedIndexTable);
+//                }
                 
-                NotifyOfPropertyChange(() => SelectedIndexTable);
-            }
-        }
+//                NotifyOfPropertyChange(() => SelectedIndexTable);
+//            }
+//        }
 
-        public void btTableSelected_Click(int SelectedListTable)
-        {
+//        public void btTableSelected_Click(int SelectedListTable)
+//        {
 
-            if(TablesList[SelectedIndexTable].Status)
-            {
-                TableDetailViewModel tableDetailViewModel = new TableDetailViewModel();
-                //orderDetailViewModel.eventChange += HandleCallBack;
+//            if(TablesList[SelectedIndexTable].Status)
+//            {
+//                TableDetailViewModel tableDetailViewModel = new TableDetailViewModel();
+//                //orderDetailViewModel.eventChange += HandleCallBack;
 
-                WindowManager windowManager = new WindowManager();
-                windowManager.ShowWindowAsync(tableDetailViewModel);
-            }
-            else if(isChoose && !TablesList[SelectedIndexTable].Status)
-            {
-                eventChooseTableToOrder?.Invoke(SelectedListTable);
-                TablesList[SelectedIndexTable].Status = true;
-                this.TryCloseAsync();
-            }
-            NotifyOfPropertyChange(() => TablesList[SelectedIndexTable].BgStatusTable);
-        }
+//                WindowManager windowManager = new WindowManager();
+//                windowManager.ShowWindowAsync(tableDetailViewModel);
+//            }
+//            else if(isChoose && !TablesList[SelectedIndexTable].Status)
+//            {
+//                eventChooseTableToOrder?.Invoke(SelectedListTable);
+//                TablesList[SelectedIndexTable].Status = true;
+//                this.TryCloseAsync();
+//            }
+//            NotifyOfPropertyChange(() => TablesList[SelectedIndexTable].BgStatusTable);
+//        }
 
-        private ObservableCollection<Table> GetAllTableList()
-        {
+//        private ObservableCollection<Table> GetAllTableList()
+//        {
             
-            return new ObservableCollection<Table>()
-            {
-                new Table(false,1,4),
-                new Table(true,2,4),
-                new Table(false,2,3),
-                new Table(true,3,4),
-                new Table(false,3,4),
-                new Table(true,3,2),
-                new Table(true,1,4),
-                new Table(false,2,4),
-                new Table(true,2,3),
-                new Table(false,3,4),
-                new Table(true,3,4),
-                new Table(true,3,2),
-            };
-        }
+//            return new ObservableCollection<Table>()
+//            {
+//                new Table(false,1,4),
+//                new Table(true,2,4),
+//                new Table(false,2,3),
+//                new Table(true,3,4),
+//                new Table(false,3,4),
+//                new Table(true,3,2),
+//                new Table(true,1,4),
+//                new Table(false,2,4),
+//                new Table(true,2,3),
+//                new Table(false,3,4),
+//                new Table(true,3,4),
+//                new Table(true,3,2),
+//            };
+//        }
 
-        private ObservableCollection<int> listFloor;
+//        private ObservableCollection<int> listFloor;
 
-        public ObservableCollection<int> ListFloor
-        {
-            get { return listFloor; }
-            set { listFloor = value; NotifyOfPropertyChange(() => ListFloor); }
-        }
+//        public ObservableCollection<int> ListFloor
+//        {
+//            get { return listFloor; }
+//            set { listFloor = value; NotifyOfPropertyChange(() => ListFloor); }
+//        }
 
-        private int listFloorSelected = 1;
+//        private int listFloorSelected = 1;
 
-        public int ListFloorSelected
-        {
-            get { return listFloorSelected; }
-            set 
-            { 
-                listFloorSelected = value; 
-                NotifyOfPropertyChange(() => ListFloorSelected);
-                TablesList = GetTableList(ListFloorSelected, TablesAllList);
-                GetStatusAllTableList(TablesList);
-                NotifyOfPropertyChange(() => TablesList);
-            }
-        }
+//        public int ListFloorSelected
+//        {
+//            get { return listFloorSelected; }
+//            set 
+//            { 
+//                listFloorSelected = value; 
+//                NotifyOfPropertyChange(() => ListFloorSelected);
+//                TablesList = GetTableList(ListFloorSelected, TablesAllList);
+//                //GetStatusAllTableList(TablesList);
+//                NotifyOfPropertyChange(() => TablesList);
+//            }
+//        }
 
-        private ObservableCollection<Table> GetTableList(int floor,ObservableCollection<Table> tablesList)
-        {
-            ObservableCollection<Table> list = new ObservableCollection<Table>();
-            for (int i = 0; i < tablesList.Count; i++)
-            {
-                if(tablesList[i].Floor == floor)
-                {
-                    list.Add(tablesList[i]);
+//        //private ObservableCollection<Table> GetTableList(int floor,ObservableCollection<Table> tablesList)
+//        //{
+//        //    ObservableCollection<Table> list = new ObservableCollection<Table>();
+//        //    for (int i = 0; i < tablesList.Count; i++)
+//        //    {
+//        //        if(tablesList[i].Floor == floor)
+//        //        {
+//        //            list.Add(tablesList[i]);
                     
-                }
+//        //        }
 
-            }
-            return list;
+//        //    }
+//        //    return list;
             
-        }
+//        //}
 
 
 
-        public ObservableCollection<Table> TablesAllList;
+//        //public ObservableCollection<Table> TablesAllList;
 
-        private ObservableCollection<Table> tables;
-        public ObservableCollection<Table> TablesList
-        {
-            get { return tables; }
-            set { tables = value; NotifyOfPropertyChange(() => TablesList); }
-        }
+//        //private ObservableCollection<Table> tables;
+//        //public ObservableCollection<Table> TablesList
+//        //{
+//        //    get { return tables; }
+//        //    set { tables = value; NotifyOfPropertyChange(() => TablesList); }
+//        //}
 
-        public void btDetail_Click()
-        {
-            TableDetailViewModel registerViewModel = new TableDetailViewModel();
-            WindowManager windowManager = new WindowManager();
-            windowManager.ShowDialogAsync(registerViewModel);
+//        public void btDetail_Click()
+//        {
+//            TableDetailViewModel registerViewModel = new TableDetailViewModel();
+//            WindowManager windowManager = new WindowManager();
+//            windowManager.ShowDialogAsync(registerViewModel);
 
-        }
+//        }
 
         
-    }
+//    }
 
     
-}
+//}
