40bc0e36155312eaf205c814c21f7f0f25a69243
17521204 <50266550+tuphan1709@users.noreply.github.com>
2022-04-07 00:24:21 +0700

    update model table

--- CoffeePos/CoffeePos/Models/Table.cs
+++ CoffeePos/CoffeePos/Models/Table.cs
@@ -12,39 +12,39 @@ namespace CoffeePos.Models
         public Dictionary<int, Table> TableNumber { get; set; }
         public class Table
         {
-            //public bool? TableStatus { get ; set; }
+            public bool TableStatus { get; set; }
             public int TableFloor { get; set; }
             public int TableSeat { get; set; }
 
-            //private SolidColorBrush? defaulColor = new SolidColorBrush(Colors.Green);
+            private SolidColorBrush defaulColor = new SolidColorBrush(Colors.Green);
 
-            //private SolidColorBrush? bgStatusTable;
-            //public SolidColorBrush? BgStatusTable
-            //{
-            //    get
-            //    {
-            //        if (TableStatus == false)
-            //        {
-            //            bgStatusTable = new SolidColorBrush(Colors.Red);
-            //        }
-            //        else
-            //        {
-            //            bgStatusTable = new SolidColorBrush(Colors.Green);
-            //        }
-            //        return bgStatusTable;
-            //    }
-            //    set
-            //    {
-            //        bgStatusTable = value;
-            //    }
-            //}
-            //public Table(bool status, int floor, int seat, SolidColorBrush bgStatusTable = default)
-            //{
-            //    TableStatus = status;
-            //    TableFloor = floor;
-            //    TableSeat = seat;
-            //    BgStatusTable = bgStatusTable;
-            //}
+            private SolidColorBrush bgStatusTable;
+            public SolidColorBrush BgStatusTable
+            {
+                get
+                {
+                    if (TableStatus == false)
+                    {
+                        bgStatusTable = new SolidColorBrush(Colors.Red);
+                    }
+                    else
+                    {
+                        bgStatusTable = new SolidColorBrush(Colors.Green);
+                    }
+                    return bgStatusTable;
+                }
+                set
+                {
+                    bgStatusTable = value;
+                }
+            }
+            public Table(bool status, int floor, int seat, SolidColorBrush bgStatusTable = default)
+            {
+                TableStatus = status;
+                TableFloor = floor;
+                TableSeat = seat;
+                BgStatusTable = bgStatusTable;
+            }
 
         }
     }
