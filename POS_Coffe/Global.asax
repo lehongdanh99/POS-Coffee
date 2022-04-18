612f73c06be545179d54f741a8ab2abaed3c9ad0
lehongdanh99 <lehongdanh99@gmail.com>
2022-04-18 18:35:46 +0700

    Revert Branch

---
+++ POS_Coffe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="POS_Coffe.MvcApplication" Language="C#" %>
---
+++ POS_Coffe/Models/RecipeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace POS_Coffe.Models
+{
+    public class RecipeModel
+    {
+        public int MaterialID { get; set; }
+        public int FoodID { get; set; }
+        public int AmountForOne { get; set; }
+        public string Size { get; set; }
+    }
+}
