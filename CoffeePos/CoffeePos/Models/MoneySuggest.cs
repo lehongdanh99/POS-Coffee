72d97d1e7b765b4dd0268191db213ab3ec1b3882
nguyen anh <17110094@student.hcmute.edu.vn>
2022-04-23 22:56:59 +0700

    add model moneysuggest

---
+++ CoffeePos/CoffeePos/Models/MoneySuggest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeePos.Models
+{
+    public class MoneySuggest
+    {
+            public int Value { get; set; }
+
+            public MoneySuggest(int value)
+            {
+            Value = value;
+            }
+    }
+}
